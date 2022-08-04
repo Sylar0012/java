3f3dfd7528406ed300d9fe879834df28c2cda3b1
Sylar <kmc42692@naver.com>
2022-08-04 18:49:44 +0900

    08.04

--- study/src/ex09/OverEx03.java
+++ study/src/ex09/OverEx03.java
@@ -1,62 +1,92 @@
 package ex09;
 
-class Zealot{
+class 유닛{
+	String getName( ) {return "";}
+	int getPower() {return 0;}
+	int getHp() {return 0;}
+	void setHp(int hp) {}
+}
+
+class Zealot extends 유닛{
+	String name = "질럿";
 	int power = 5;
 	int hp = 100;
+	String getName( ) {return name;}
+	int getPower() {return power;}
+	int getHp() {return hp;}
+	void setHp(int hp) {
+		this.hp = hp;
+	}	
 }
 
-class Dragoon{
+class Dragoon extends 유닛{
+	String name = "드라군";
 	int power = 10;
 	int hp = 100;
+	String getName( ) {return name;}
+	int getPower() {return power;}
+	int getHp() {return hp;}
+	void setHp(int hp) {
+		this.hp = hp;
+	}	
 }
 
-class River{
+
+
+class River extends 유닛{
+	String name = "리버";
 	int power = 50;
 	int hp = 100;
+	String getName( ) {return name;}
+	int getPower() {return power;}
+	int getHp() {return hp;}
+	void setHp(int hp) {
+		this.hp = hp;
+	}	
+	
+	
 }
 
+class DarkTempler extends 유닛{
+	String name = "다크템플러";
+	int power = 30;
+	int hp = 100;
+	String getName( ) {return name;}
+	int getPower() {return power;}
+	int getHp() {return hp;}
+	void setHp(int hp) {
+		this.hp = hp;
+	}	
+	
+	
+}
+	
 public class OverEx03 {
-	// 질럿이 드라군을 공격
-	static void attack(Zealot u1, Dragoon u2) {
-		u2.hp -= u1.power;
-		System.out.println("드라군의 hp는 : "+u2.hp);
-	}
-	// 드라군이 질럿을 공격
-	static void attack(Dragoon u2,Zealot u1) {
-		u1.hp -= u2.power;
-		System.out.println("질럿의 hp는 : "+u1.hp);
-	}
-	// 질럿이 리버를 공격
-	static void attack(Zealot u1, River u3) {
-		u3.hp -= u1.power;
-		System.out.println("리버의 hp는 : "+u3.hp);
-	}
-	// 리버가 질럿을 공격
-	static void attack(River u3,Zealot u1) {
-		u1.hp -= u3.power;
-		System.out.println("질럿의 hp는 : "+u1.hp);
-	}
-	// 드라군이 리버를 공격
-	static void attack(Dragoon u2,River u3) {
-		u3.hp -= u2.power;
-		System.out.println("리버의 hp는 : "+u3.hp);
-	}
-	// 드라군이 질럿을 공격
-	static void attack(River u3, Dragoon u2) {
-		u2.hp -= u3.power;
-		System.out.println("드라군의 hp는 : "+u2.hp);
+	
+	static void attack(유닛 u1, 유닛 u2) {
+		String n = u1.getName();
+		u1.setHp(u1.getHp()-u2.getPower());
+		System.out.println(u1.getName()+"의 남은 체력 : "+u1.getHp());
 	}
 	
+	
 	public static void main(String[] args) {
-		Zealot z1 = new Zealot();
-		Dragoon d1 = new Dragoon();
-		River l1 = new River();
+		유닛 z1 = new Zealot();
+		유닛 d1 = new Dragoon();
+		유닛 l1 = new River();
+		유닛 d3 = new DarkTempler();
 		
 		attack(z1,d1);
-		attack(d1,z1);
 		attack(z1,l1);
+		attack(d1,z1);
+		attack(d1,l1);
 		attack(l1,z1);
 		attack(l1,d1);
-		attack(d1,l1);
+		attack(d3,d1);
+		attack(d1,d3);
+		
+		
+
+		
 	}
 }
---
+++ study/src/ex10/ExtendsEx04.java
@@ -0,0 +1,28 @@
+package ex10;
+
+class 햄버거 {
+	String name = "햄버거";
+	
+	String getName() {
+		return "하하하";
+	}
+}
+
+class 더블치즈햄버거 extends 햄버거{
+	String name = "더블치즈햄버거";
+	
+	String getName() {
+		return name;
+	}
+}
+
+public class ExtendsEx04 {
+	
+	public static void main(String[] args) {
+		햄버거 s =new 더블치즈햄버거();
+		System.out.println(s.name);
+		System.out.println(s.getName());
+		
+	}
+
+}
