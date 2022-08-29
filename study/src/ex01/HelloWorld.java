3d20d488bc66a61e1682c68c5b8d6870b0a1c93f
Sylar <kmc42692@naver.com>
2022-08-29 09:45:51 +0900

    08.29

--- study/src/ex01/HelloWorld.java
+++ study/src/ex01/HelloWorld.java
@@ -5,7 +5,6 @@ public class HelloWorld {
 	public static void main(String[] args) {
 
 		System.out.println("Hello World");
-		
-		System.out.println("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"); // 깃 토큰
+	
 	}
 }
